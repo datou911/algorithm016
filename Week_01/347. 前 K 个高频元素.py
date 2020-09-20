74460868fa783a03475790a0d62687ad5a5f0dc7
Qinyoung <46709626+datou911@users.noreply.github.com>
2020-09-20 23:01:33 +0800

    Delete 347. 前 K 个高频元素.py

--- Week_01/347. 前 K 个高频元素.py
+++
@@ -1,7 +0,0 @@
-# Created by Qin_young on 2020/9/20 22:08 
-# coding = utf-8
-from collections import Counter
-
-class Solution:
-    def topKFrequent(self, nums, k: int):
-        return [i[0] for i in Counter(nums).most_common(k)]
